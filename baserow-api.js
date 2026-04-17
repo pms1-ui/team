355a5850754ba464f17082968bb670d455a02b98
박명수 <pms1@childy.co.kr>
2026-04-17 13:08:07 +0900

    Baserow API 토큰 업데이트

--- baserow-api.js
+++ baserow-api.js
@@ -1,7 +1,7 @@
 // Baserow API Configuration
 const BASEROW_CONFIG = {
     apiUrl: 'https://baserow.childylab.com/api',
-    token: 'Token 9Q67Pj5Lz061Rpx6pk30wX6RZxQ7ko3d',
+    token: 'Token 5Z9IZSqCKhcBDXf5B20RD1KTkIwqxvGy',
     tables: {
         divisions: 1940,
         teams: 1941,
